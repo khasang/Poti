5914ddc22b130ec746ded6b4f7fac94687fc4f2e
ni032mas <ni032mas@mail.ru>
2015-12-11 18:33:35 +0300

    Изменил анимацию списка настроек

--- wear/src/main/java/com/ni032mas/poti/TimersWearableListView.java
+++ wear/src/main/java/com/ni032mas/poti/TimersWearableListView.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.support.wearable.view.WearableListView;
 import android.util.AttributeSet;
 
-/**
- * Created by aleksandr.marmyshev on 03.12.2015.
- */
 public class TimersWearableListView extends WearableListView {
     boolean isScroll;
     public TimersWearableListView(Context context) {
